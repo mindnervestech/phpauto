73bb60e81d8b0ebdc97cf206642fb6f156e4c0c0
unknown <swapnil.nale22@gmail.com>
2016-03-22 19:17:28 +0530

    auto BE

--- public/angularjs/app/dashboard/dashboardCtrl.js
+++ public/angularjs/app/dashboard/dashboardCtrl.js
@@ -2807,6 +2807,7 @@ angular.module('newApp')
 	    			$scope.lead.stockWiseData = $scope.stockWiseData;
 	    			console.log($scope.lead);
 	    			$http.post('/createLead',$scope.lead).success(function(response) {
+	    				
 	    				$scope.getVisitedData('week','countHigh','0','0','All');
 	    				$scope.userLocationData('Week','person');
 	    				$scope.getAllSalesPersonRecord($scope.salesPerson);
