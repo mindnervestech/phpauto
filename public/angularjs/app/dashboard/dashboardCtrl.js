3590b39faa11bea4b1f6f9ffc2c54481beb43f65
unknown <dineshkudale2@gmail.com>
2016-04-25 18:20:17 +0530

    date

--- public/angularjs/app/dashboard/dashboardCtrl.js
+++ public/angularjs/app/dashboard/dashboardCtrl.js
@@ -2898,10 +2898,12 @@ angular.module('newApp')
 	    				var notifContent;
 	    				angular.forEach(data, function(value, key) {
 	    				
+	    					console.log($filter('date')(value.confirmTime * 1000,"hh:mm a");
+	    					console.log(">>>>>>>>>>>>>");
 	    					value.confirmDate = $filter('date')(value.confirmDate,"MM-dd-yyyy");
 	    					value.confirmTime = $filter('date')(value.confirmTime,"hh:mm a");
 	    					   
-	    					
+	    					console.log(value.confirmTime);
 	    				notifContent = "<div class='alert alert-dark media fade in bd-0' id='message-alert'><div class='media-left'></div><div class='media-body width-100p'><p class='row' style='margin-left:0;'><span>"+value.name+" information has been changed</span><br><span>"+value.confirmDate+"   "+value.confirmTime+"</span><br><span>"+value.reason+"</span></p><p class='row' style='margin-left:0;'></p><p class='pull-left' style='margin-left:65%;'><a class='f-12'>Close&nbsp;<i></i></a></p></div></div>";
 	    				
 	    				var position = 'topRight';
