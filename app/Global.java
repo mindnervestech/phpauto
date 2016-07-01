67c742a5b97c977dab0b502b5e986ee8509c0a17
“abc” <harshad.r.gojare@gmail.com>
2016-07-01 21:35:18 +0530

    edit cha

--- app/Global.java
+++ app/Global.java
@@ -1,4 +1,6 @@
+import play.Application;
 import play.GlobalSettings;
+import play.Logger;
 import play.api.mvc.Result;
 import play.libs.F.Promise;
 import play.mvc.Action;
@@ -10,7 +12,10 @@ public class Global extends GlobalSettings {
 	public static final String APP_ENV_LOCAL = "local";
 	public static final String APP_ENV_VAR = "CURRENT_APPNAME";
 	
-	// For CORS
+	/*public <T extends EssentialFilter> Class<T>[] filters() {
+        return new Class[]{GzipFilter.class};
+    }*/
+	
 	private class ActionWrapper extends Action.Simple {
 		public ActionWrapper(Action<?> action) {
 			this.delegate = action;
@@ -29,6 +34,16 @@ public class Global extends GlobalSettings {
 	public Action<?> onRequest(Http.Request request, java.lang.reflect.Method actionMethod) {
 		return new ActionWrapper(super.onRequest(request, actionMethod));
 	}
+	
+	@Override
+	public void onStart(Application app) {
+	//	NewsLetter.newsletterSchedulling();
+	//	TestDriveScheduler.newsletterSchedulling();
+		System.err.println("Testing..");
+	//	MeetingAndTestDriveScheduler.meetingSchedulling();
+	//	ClickyDataScheduler.clickySchedulling();
+	}
+	
 	
 	@Override
 	public void onStop(Application app) {
